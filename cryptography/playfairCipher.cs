2deeb1c5fffadaef3e290b428aca01fb5f0c3dbf
Simon <srainslie@protonmail.com>
2021-05-10 16:17:40 +0100

    Correcting playfair bugs so it works as expected.

--- cryptography/playfairCipher.cs
+++ cryptography/playfairCipher.cs
@@ -8,6 +8,7 @@ namespace cryptography
     public class playfairCypher 
     {
         public void run(){
+            Console.WriteLine("Notes: ");
             Console.WriteLine("Select option: ");
             Console.WriteLine("1. Encode ");
             Console.WriteLine("2. Decode ");
@@ -38,7 +39,7 @@ namespace cryptography
         {
             var fileString = File.ReadAllText("files/input.txt");
             var message = prepMessage(fileString);
-            var output = ReplaceVals(message, key);
+            var output = ReplaceVals(message, key, true);
             Console.WriteLine(output);
             File.WriteAllTextAsync("files/output.txt", output);
         }
@@ -46,18 +47,26 @@ namespace cryptography
         private void decode (char[,] key) {
             var fileString = File.ReadAllText("files/output.txt");
             var message = dSplitMessage(fileString);
-            var output = ReplaceVals(message, key);
+            var output = ReplaceVals(message, key, false);
             Console.WriteLine(output);
             File.WriteAllTextAsync("files/decoded.txt", output);
         }
 
-        private string ReplaceVals(List<KeyValuePair<char, char>> message, char[,] key)
+        private string ReplaceVals(List<KeyValuePair<char, char>> message, char[,] key, bool encrypt)
         {
             var encryptedPairs = new List<KeyValuePair<char,char>>();
             var encryptedMessage = "";
             foreach (var pair in message)
             {
-                encryptedPairs.Add(replacePair(pair, key));
+                //Console.WriteLine("pair.key: " + pair.Key + " && pair.value: " + pair.Value);
+                var replacement = new KeyValuePair<char,char>();
+                if (encrypt) {
+                    replacement = eReplacePair(pair,key);
+                } else {
+                    replacement = dReplacePair(pair,key);
+                }
+                //Console.WriteLine("replacement.key: " + replacement.Key + " && replacement.value: " + replacement.Value);
+                encryptedPairs.Add(replacement);
             }
 
             foreach (var pair in encryptedPairs)
@@ -66,8 +75,7 @@ namespace cryptography
             }
             return encryptedMessage;
         }
-
-        private KeyValuePair<char,char> replacePair(KeyValuePair<char,char> pair, char[,] key) 
+        private KeyValuePair<char,char> dReplacePair(KeyValuePair<char,char> pair, char[,] key) 
         {
             var first = new KeyValuePair<int, int>();
             var second = new KeyValuePair<int, int>();
@@ -98,7 +106,68 @@ namespace cryptography
                 }
             }
 
-            // If same row
+            //Same Row
+            if (first.Key == second.Key){
+                if (first.Value != 0 && second.Value != 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[first.Key, first.Value-1],key[second.Key, second.Value-1]);
+                } else if(first.Value == 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[first.Key, 4],key[second.Key, second.Value-1]);
+                } else if (second.Value == 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[first.Key, first.Value-1],key[second.Key, 4]);
+                }
+            }
+            //Same Col
+            else if (first.Value == second.Value)
+            {
+                if (first.Key != 0 && second.Key != 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[first.Key-1, first.Value],key[second.Key-1, second.Value]);
+                } else if(first.Key == 0 && second.Key != 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[4, first.Value],key[second.Key-1, second.Value]);
+                } else if (second.Key == 0 && first.Key != 0){
+                    encryptedPair = new KeyValuePair<char, char>(key[first.Key-1, first.Value],key[4, second.Value]);
+                } else if (first.Key == 0 && second.Key == 0){
+                    Console.WriteLine("ERROR");
+                }
+            } else{
+                Console.WriteLine("box");
+                encryptedPair = new KeyValuePair<char, char>(key[first.Key, second.Value],key[second.Key, first.Value]);
+            }
+
+            return encryptedPair;
+        }
+
+        private KeyValuePair<char,char> eReplacePair(KeyValuePair<char,char> pair, char[,] key) 
+        {
+            var first = new KeyValuePair<int, int>();
+            var second = new KeyValuePair<int, int>();
+            var encryptedPair = new KeyValuePair<char, char>();
+            var counter = 0;
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (key[row, col] == pair.Key)
+                    {
+                        first = new KeyValuePair<int, int> (row,col);
+                        counter++;
+                    } else if (key[row, col] == pair.Value) 
+                    {
+                        second = new KeyValuePair<int, int> (row, col);
+                        counter++;
+                    }
+                    if (counter == 2)
+                    {
+                        break;
+                    }
+                }
+
+                if (counter == 2)
+                {
+                    break;
+                }
+            }
+
+            // Same row
             if (first.Key == second.Key){
                 if (first.Value != 4 && second.Value != 4){
                     encryptedPair = new KeyValuePair<char, char>(key[first.Key, first.Value+1],key[second.Key, second.Value + 1]);
@@ -108,16 +177,18 @@ namespace cryptography
                     encryptedPair = new KeyValuePair<char, char>(key[first.Key, first.Value+1],key[second.Key, 0]);
                 }
             }
+            //Same Col
             else if (first.Value == second.Value)
             {
                 if (first.Key != 4 && second.Key != 4){
                     encryptedPair = new KeyValuePair<char, char>(key[first.Key+1, first.Value],key[second.Key+1, second.Value]);
-                } else if(first.Value == 4){
-                    encryptedPair = new KeyValuePair<char, char>(key[0, first.Value],key[second.Key, second.Value + 1]);
-                } else if (second.Value == 4){
+                } else if(first.Key == 4 && second.Key != 4){
+                    encryptedPair = new KeyValuePair<char, char>(key[0, first.Value],key[second.Key+1, second.Value]);
+                } else if (second.Key == 4 && first.Key != 4){
                     encryptedPair = new KeyValuePair<char, char>(key[first.Key+1, first.Value],key[0, second.Value]);
+                } else if (first.Key == 4 && second.Key == 4){
+                    Console.WriteLine("ERROR");
                 }
-                // same col
             } else{
                 encryptedPair = new KeyValuePair<char, char>(key[first.Key, second.Value],key[second.Key, first.Value]);
             }
@@ -130,13 +201,28 @@ namespace cryptography
             var osb = new StringBuilder(inputMessage.ToLower());
             var csb = new StringBuilder(inputMessage.ToLower());
 
-            var llocs = sharedLib.GetCharIndexInString(osb.ToString(), (char)'j');
+            //Find and replace all 'j' chars with 'i'
+            var jlocs = sharedLib.GetCharIndexInString(osb.ToString(), (char)'j');
 
-            foreach (var lloc in llocs)
+            foreach (var jloc in jlocs)
             {
-                csb.Remove(lloc,1);
-                csb.Insert(lloc,(char)'i');
+                csb.Remove(jloc,1);
+                csb.Insert(jloc,(char)'i');
             }
+            
+            //Find and remove all whitespace char
+            //Counter required as sb length changes unlike array
+            var counter = 0;
+            var chararray = osb.ToString().ToCharArray();
+            for (int i = 0; i < chararray.Length; i++)
+            {
+                
+                if(!Char.IsLetter(chararray[i])){
+                    csb.Remove(i-counter,1);
+                    counter++;
+                }
+            }
+
             var couplets = eSplitMessage(csb);
             return couplets;
         }
